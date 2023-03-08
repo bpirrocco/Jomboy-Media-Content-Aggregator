faa517a762b2da02c3763d29b121fd083d34bba2
Brandon Pirrocco <bpirrocco@gmail.com>
2023-03-07 23:25:06 -0500

    save_new_episode function for multiple feed parsin

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -6,21 +6,32 @@ from dateutil import parser
 from podcasts.models import Episode
 
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image["href"]
+def save_new_episodes(feed):
+    """Saves new episodes to the database.
 
-        for item in feed.entries:
-            if not Episode.objects.filter(guid=item.guid).exists():
-                episode = Episode(
-                    title=item.title,
-                    description=item.description,
-                    pub_date=parser.parse(item.published),
-                    link=item.link,
-                    image=podcast_image,
-                    podcast_name=podcast_title,
-                    guid=item.guid,
-                )
-                episode.save()
+
+    Checks the episode GUID against the episodes currently stored in the
+
+    database. If not found, then a new `Episode` is added to the database.
+
+
+    Args:
+
+        feed: requires a feedparser object
+
+    """
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image["href"]
+
+    for item in feed.entries:
+        if not Episode.objects.filter(guid=item.guid).exists():
+            episode = Episode(
+                title=item.title,
+                description=item.description,
+                pub_date=parser.parse(item.published),
+                link=item.link,
+                image=podcast_image,
+                podcast_name=podcast_title,
+                guid=item.guid,
+            )
+            episode.save()
