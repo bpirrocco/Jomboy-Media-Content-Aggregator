f1b4e2779113fe9513325019e478ef31ad8995d8
Brandon Pirrocco <bpirrocco@gmail.com>
2023-03-20 18:54:48 -0400

    Run migrations for social_django app

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     # MyApps
     'podcasts.apps.PodcastsConfig',
     # Third Party Apps
-    'django_apscheduler'
+    'django_apscheduler',
+    'social_django'
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
         },
     },
