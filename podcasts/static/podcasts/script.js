ba0ef979d73ab2cae308017dc156a439ab990a3f
Brandon Pirrocco <bpirrocco@gmail.com>
2023-03-25 17:32:14 -0400

    Some scripting for user dashboard

--- podcasts/static/podcasts/script.js
+++ podcasts/static/podcasts/script.js
@@ -4,9 +4,20 @@ if (window.location.pathname == '/podcasts/') {
         currentBtn.addEventListener('click', function() {
             favorite(currentBtn);
         });
-    })
+    });
 }
 
 function favorite(btn) {
     btn.classList.replace("btn-outline-danger", "btn-danger");
+}
+
+// *******************
+// DASHBOARD SCRIPTING
+// *******************
+
+
+function selectView() {
+    const select = document.getElementById('dashboard');
+    const view = select.value;
+    window.location.href = `${view}/`
 }
