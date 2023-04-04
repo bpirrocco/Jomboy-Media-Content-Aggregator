925c04919e5c2e0943c0745b4a55326ba10271f8
Brandon Pirrocco <bpirrocco@gmail.com>
2023-04-04 18:49:17 -0400

    Register content admin

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Episode
+from .models import Episode, Content
 
 @admin.register(Episode)
 class EpisodeAdmin(admin.ModelAdmin):
     list_display = ("podcast_name", "title", "pub_date")
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ("name", "content_type", "categories")
