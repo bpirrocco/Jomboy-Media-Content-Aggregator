e1080a942d79cda9533d22255f6b2e4a51691660
Brandon Pirrocco <bpirrocco@gmail.com>
2023-04-04 18:42:11 -0400

    Create content model, run migrations

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Episode(models.Model):
     title = models.CharField(max_length=200)
@@ -8,15 +9,27 @@ class Episode(models.Model):
     link = models.URLField()
     image = models.URLField()
     podcast_name = models.CharField(max_length=100)
-    favorite = models.ManyToManyField(User, related_name="favorite", default=None, blank=True)
+    favorite = models.ManyToManyField(User, related_name="episode_favorite", default=None, blank=True)
     guid = models.CharField(max_length=50)
 
     def __str__(self) -> str:
         return f"{self.podcast_name}: {self.title}"
 
 class Content(models.Model):
+    PODCAST = "PC"
+    YOUTUBE = "YT"
+    CONTENT_TYPE_CHOICES = [
+        (PODCAST, "Podcast"),
+        (YOUTUBE, "Youtube")
+    ]
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.URLField()
     categories = models.TextField()
-    
+    link = models.URLField()
+    content_type = models.CharField(max_length=7, choices=CONTENT_TYPE_CHOICES)
+    favorite = models.ManyToManyField(User, related_name="content_favorite", default=None, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.content_type}: {self.name}"
