0523b917658204682bc7fe7f8502c8e691d5aebc
Brandon Pirrocco <bpirrocco@gmail.com>
2023-04-18 23:12:03 -0400

    Script refactoring

--- podcasts/static/podcasts/script.js
+++ podcasts/static/podcasts/script.js
@@ -1,29 +1,59 @@
-if (window.location.pathname == '/podcasts/') {
-    const buttons = document.querySelectorAll('.favorite');
-    buttons.forEach(function(currentBtn){
-        currentBtn.addEventListener('click', function() {
-            favorite(currentBtn);
-        });
-    });
-}
-
-function favorite(btn) {
-    btn.classList.replace("btn-outline-danger", "btn-danger");
-}
-
-// *******************
-// DASHBOARD SCRIPTING
-// *******************
-
-
-// function selectView() {
-//     const select = document.getElementById('dashboard');
-//     const view = select.value;
-//     window.location.href = `${view}/`
+// if (window.location.pathname == '/podcasts/') {
+//     const buttons = document.querySelectorAll('.favorite');
+//     buttons.forEach(function(currentBtn){
+//         currentBtn.addEventListener('click', function() {
+//             favorite(currentBtn);
+//         });
+//     });
 // }
 
+// function favorite(btn) {
+//     btn.classList.replace("btn-outline-danger", "btn-danger");
+// }
 
-// ****************
-// IFRAME SCRIPTING
-// ****************
+const app = {
 
+    init: () => {
+        document.addEventListener('DOMContentLoaded', app.load);
+        console.log('HTML loaded');
+    },
+
+    load: () => {
+        app.runScript();
+    },
+
+    runScript: () => {
+        let page = document.getElementsByTagName('main');
+        page = page[0].id;
+
+        switch (page) {
+            case 'podcasts':
+                app.favoriteBtn();
+            default:
+                app.pass();
+        }
+    },
+
+    pass: () => {
+        return
+    },
+
+
+    // **********************
+    // Content List Functions
+    // **********************
+
+
+    favorite: (btn) => {
+        btn.classList.replace("btn-outline-danger", "btn-danger");
+    },
+
+    favoriteBtn: () => {
+        const buttons = document.querySelectorAll('.favorite');
+        buttons.forEach(function(currentBtn){
+            currentBtn.addEventListener('click', function() {
+                app.favorite(currentBtn);
+            });
+        });
+    },
+}
